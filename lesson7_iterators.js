daf1bd19bed0cbc8872a2019ba6b84725fc3416a
daahny <daniel.androussenko@outlook.com>
2021-08-01 20:39:08 -0700

    lesson 7 part 1

---
+++ lesson7_iterators.js
@@ -0,0 +1,7 @@
+// functions are first-class objects
+// they are like other objects, they have properties and methods
+// every function is a Function object
+
+function funct() {}
+let funct_var = funct;
+// funct and funct_var both hold the address of the function. they refer to the same data
