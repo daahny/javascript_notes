760bd3c8d74c5bcb3d59d94fdb7c5043f99f49a3
daahny <daniel.androussenko@outlook.com>
2021-08-06 00:13:00 -0700

    node modules

--- lesson10_modules.js
+++ lesson10_modules.js
@@ -9,6 +9,7 @@
 // Using Node to implement modules
 // create the file where functions are declared
 // add the functions as properties to the built-in module.exports object
+// *-- assume this file is converters.js --*
 function celsiusToFahrenheit(celsius) {
     return celsius * (9/5) + 32;
   }
@@ -16,3 +17,16 @@ function celsiusToFahrenheit(celsius) {
 module.exports.celsiusToFahrenheit = celsiusToFahrenheit;
 // module.exports is an object built into the Node runtime environment.
 // other files import this object, and through this object they can access other functions
+// using the require() function:
+const converters = require('./converters.js');
+// at this point, the module.exports object is now passed by reference to the converters variable
+// this way, the converters variable can refer to celsiusToFahrenheit():
+converters.celsiusToFahrenheit(0);
+converters.celsiusToFahrenheit(100);
+
+
+// Object destructuring to be selective with require()
+// Import a specific function like so:
+const { celsiusToFahrenheit } = require('./converters.js');
+const celsiusInput = process.argv[2];
+const gehrenheitValue = celsiusToFahrenheit(celsiusInput);
