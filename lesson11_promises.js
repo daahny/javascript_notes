815c4cf0634c9853a17834262c57b0e1de47ffc1
daahny <daniel.androussenko@outlook.com>
2021-08-12 00:36:12 -0700

    lesson12 and lesson13

--- lesson11_promises.js
+++ lesson11_promises.js
@@ -1,3 +1,20 @@
+// The event loop in JS is the secret to the illusion of concurrency
+// The event loop describes a loop that occurs among data structures and APIs.
+
+// The call stack is a stack that holds the function calls for a single operation
+// The event/message queue is a queue that holds operations to be completed
+// When an operation in the call stack has completed and there are no more function calls,
+// the event queue will send the oldest event/message/operation to the call stack for processing
+
+// The event loopo also has runtime environment APIs (the web API or Node API) to server as another thread.
+// A JS script is a set of operations running in a single thread. However, when JS encounters an operation
+// that needs/should/can be done asynchronously, it will send that operation to the API for processing.
+// When the API finishes, it will send this task back to the event queue.
+// https://www.educative.io/edpresso/what-is-an-event-loop-in-javascript
+// Refer the event_loop.png for how the setTimeout function works and why the specified delay
+// is a minimum value rather than a gauranteed value
+
+
 // asynchronous operation - a task that runs concurrently with other tasks (on the side)
 
 // A Promise object - represents the eventual outcome of an operation
@@ -18,7 +35,7 @@
 // if reject() is invoked, status is changed to Rejected and the rejection reason becomes the argument passed into reject()
 
 const executorFunction = (resolve, reject) => {
-    if (someCondition) {
+    if (true) {
         resolve('I resolved!');
     } else {
         reject('I rejected!'); 
@@ -26,3 +43,60 @@ const executorFunction = (resolve, reject) => {
   }
 
 const myFirstPromise = new Promise(executorFunction);
+
+
+// setTimeout() an asynchronous function provided by the Node api
+setTimeout(() => console.log('it has been atleast 3 seconds'), 3000);
+
+
+// .then() is a Promise method that determines the next action to take after the promise is settled
+// it takes 2 callback functions (called handlers)
+// the first handler is the success/onFulfilled handler for logic after a promise resolves
+// the other handler is the failure/onRejected handler for lagic after a promise rejects
+// if an appropriate handler is not provided, .then() will return a Promise with the same settled value it had when it was passed
+
+// Example
+const inventory = {
+    sunglasses: 9000,
+    watermelons: 0
+};
+
+function exec(resolve, reject) {
+    if (inventory.sunglasses > 0){
+        console.log('we still got sunglasses');
+        resolve('we still got sunglasses');
+    } else {
+        reject('nah we out of sunglasses');
+    }
+};
+
+const promise = new Promise(exec);
+
+function successHandler(resolvedValue) {
+    console.log("success: " + resolvedValue);
+}
+function failureHandler(rejectedValue) {
+    console.log(rejectedValue);
+}
+
+// .catch() is used for the failure handler to separate the handlers like so:
+promise.then(successHandler).catch(failureHandler);
+
+
+// Promise Chaining
+// the .then() function returns a promise
+// if the appropriate event handler is not provided, it will return a promise with 
+// the same settled value as the promise it was called with
+// The fact that .then() returns a Promise, we can change the return value and chain Promises
+// to execute in sequence
+// Note, since the first parameter is the successHandler, it is only called if the promise was successfull.
+
+p1 = new Promise(exec);
+p1.then((resolvedValue) => {new Promise(exec)})
+  .then((resolvedValue) => {new Promise(exec)}, null);
+
+
+// Promises.all()
+// Achieve concurrency by accepting an array of promises to complete in no particular order
+// If every promise resolves, Promise.all() will resolve with an array containing the resolve value from each promise
+// If a single promise rejects, a single promise returns with the rejection reason
